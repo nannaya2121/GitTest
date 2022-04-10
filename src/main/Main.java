d37703bd789abcebc8ecb2ba812dd5960e11309a
GD <nannaya2121@hanmail.net>
2022-04-10 15:29:45 +0900

    Merge 직전

--- src/main/Main.java
+++ src/main/Main.java
@@ -20,13 +20,7 @@ public class Main {
 		System.out.println("Git Hub 가서 Repository 하나 생성 후 그곳에 등록하기");
 		
 		System.out.println("브랜치 따서 계산기 모듈 구현해보기");
-		Calculator calc = new Calculator();
-		int addResult = calc.add(1, 2);
-		System.out.println(addResult);
-		System.out.println("모듈이 완료되면 switch to 에 가서 다시 master로 변경 뒤 merge 하기");
-		System.out.println("또다른 모듈 개발 완료");
-		int subResult = calc.subtract(2, 1);
-		System.out.println(subResult);
+		
 		
 		
 		
