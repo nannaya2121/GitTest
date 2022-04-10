b7235e11e14da8ad76f2c94486a8bab71a477c9f
GD <nannaya2121@hanmail.net>
2022-04-10 14:59:59 +0900

    첫번째 커밋.

---
+++ src/main/Main.java
@@ -0,0 +1,21 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		System.out.println("프로젝트 시작");
+		System.out.println("share project 완료");
+		System.out.println("commit 하면 Git Staging 창이 열림.");
+		System.out.println("항목들 중에는 필요없는 파일들이 존재.");
+		System.out.println("Git Ignore 파일에 불필요한 파일들을 추가");
+		System.out.println("gitignore.io 사이트에 가면 자동으로 등록될 수 있도록 도와줌.");
+		System.out.println("각종 운영체제, 이클립스, 자바, 빌드툴을 작성 후 생성 버튼을 클릭");
+		System.out.println(".gitignore 파일에 복사붙여넣기");
+		System.out.println(".settings .classpath .project 도 .gitignore파일에 추가하기");
+		System.out.println("Add allfiles 해서 staged Changes 로 변경하기");
+		
+		
+	}
+
+}
